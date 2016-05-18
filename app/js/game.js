4e4101a65d39db96632aa2bb25d13ec4382f8501
Katarzyna Bak <Katarzyna_Bak@epam.com>
2016-05-18 16:50:45 +0200

    implemented selecting fields

--- app/js/game.js
+++ app/js/game.js
@@ -3,7 +3,17 @@
 var Game = (function () {
   "use strict";
 
-  var myBoard = document.querySelector("#my-field");
+  var myBoard = document.querySelector("#my-field"),
+
+    // states
+    SHIPS_INIT,
+    PLAYER1_TURN,
+    PLAYER2_TURN,
+    GAMEOVER,
+    config = {
+      mode: null,
+      state: null
+    };
 
   function drawField(board) {
 
@@ -21,15 +31,39 @@ var Game = (function () {
   function hitField(e) {
     e.stopPropagation();
     var target = e.target || e.srcElement;
-    console.log(target.classList);
-    target.classList.add("burned");
+    //var target = document.elementFromPoint(e.pageX, e.pageY);
+    //target.classList.add("burned");
+    target.classList.add("missed");
+  }
 
+
+
+  function selectShip(e) {
+    e.stopPropagation();
+    var target = e.target || e.srcElement;
+    target.classList.add("ship");
+  }
+
+  function endShip(e) {
+    myBoard.removeEventListener("mousemove", selectShip);
+    myBoard.removeEventListener("mouseup", endShip);
+  }
+
+  function setShip(e) {
+    selectShip(e);
+    myBoard.addEventListener("mousemove", selectShip);
+    myBoard.addEventListener("mouseup", endShip);
   }
 
   function init() {
     drawField(myBoard);
-    myBoard.addEventListener("click", hitField);
+    //myBoard.addEventListener("click", hitField);
 
+    //myBoard.addEventListener("mousemove", Pointer.set);
+    //myBoard.addEventListener("mouseover", Pointer.show);
+    //myBoard.addEventListener("mouseout", Pointer.hide);
+
+    myBoard.addEventListener("mousedown", setShip);
   }
 
   return {
